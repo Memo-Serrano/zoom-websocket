957fe4fa297173e6ea96bad4e4f69b2c3d2e964d
Gabriel Rincon <gerz2137@gmail.com>
2025-01-14 00:00:31 -0700

    Adde Sign

--- src/public/script.js
+++ src/public/script.js
@@ -23,7 +23,7 @@ client.init({
 
 client.join({
   sdkKey: 'UEEbOouT0wWC14opf66w',
-  signature: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBLZXkiOiJVRUViT291VDB3V0MxNG9wZjY2dyIsImlhdCI6MTczNjgzNzYyNTIyMiwiZXhwIjoxNzM2ODQyNjI1MjIyLCJ0cGMiOiIxMjM0NTY3ODkiLCJyb2xlIjoxfQ.Kio4E5vsGBB-4EPaej9Ioly-k9m57kXLgmM67WvRd1A',
+  signature: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJhcHBLZXkiOiJVRUViT291VDB3V0MxNG9wZjY2dyIsImlhdCI6MTczNjgzNzk3NTU3MiwiZXhwIjoxNzM2ODQyOTc1NTcyLCJ0cGMiOiIxMjM0NTY3ODkiLCJyb2xlIjoxfQ.hNNFxMH6WzE3pIYFAvMhRGVlOOhlDUywA25xNKJJpGM',
   meetingNumber: '87423967832',
   password: '718510',
   userName: 'Invitado',
