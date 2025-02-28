2114365f00afae3ae27f4ebf983de8061eafbf40
Gabriel Rincon <gerz2137@gmail.com>
2025-02-28 11:06:37 -0700

    Panel

--- src/public/script-page2.js
+++ src/public/script-page2.js
@@ -9,59 +9,107 @@ function toggleElement(elementId, visible) {
   socket.send(JSON.stringify({ action: 'toggleElement', element: elementId, visible }));
 }
 
+socket.onmessage = (event) => {
+  const data = JSON.parse(event.data);
+  if (data.action === 'initialize') {
+    Object.keys(data.states).forEach((id) => {
+      updateElement(document.querySelector(`.show-${id}`), data.states[id]);
+    });
+
+    /* Object.keys(data.timers).forEach((id) => {
+      updateTimerDisplay(id, data.timers[id].remainingTime);
+      if (data.timers[id].running) {
+        startTimer(id, data.timers[id].remainingTime);
+      } 
+    }); */ //******Use this section to show preview of remining time*****
+  }
+};
+// Actualizar la visibilidad de un elemento
+function updateElement(element, visible) {
+  element.checked = visible;
+}
+
 // Eventos para controlar los botones
 document.getElementById('toggleButton1').addEventListener('click', () => {
-  toggleElement('cta1', true);
+  if(!document.getElementById('toggleButton1').checked) {
+    toggleElement('cta1', false);
+  } else {
+    toggleElement('cta1', true);
+  }
 });
 
-document.getElementById('hideButton1').addEventListener('click', () => {
+/* document.getElementById('hideButton1').addEventListener('click', () => {
   toggleElement('cta1', false);
-});
+}); */
 
 document.getElementById('toggleButton2').addEventListener('click', () => {
-  toggleElement('cta2', true);
+  if(!document.getElementById('toggleButton2').checked) {
+    toggleElement('cta2', false);
+  } else {
+    toggleElement('cta2', true);
+  }
 });
 
-document.getElementById('hideButton2').addEventListener('click', () => {
+/* document.getElementById('hideButton2').addEventListener('click', () => {
   toggleElement('cta2', false);
-});
+}); */
 
 document.getElementById('toggleButton3').addEventListener('click', () => {
-  toggleElement('timer1', true);
+  if(!document.getElementById('toggleButton3').checked) {
+    toggleElement('timer1', false);
+  } else {
+    toggleElement('timer1', true);
+  }
 });
 
-document.getElementById('hideButton3').addEventListener('click', () => {
+/* document.getElementById('hideButton3').addEventListener('click', () => {
   toggleElement('timer1', false);
-});
+}); */
 
 document.getElementById('toggleButton4').addEventListener('click', () => {
-  toggleElement('timer2', true);
+  if(!document.getElementById('toggleButton4').checked) {
+    toggleElement('timer2', false);
+  } else {
+    toggleElement('timer2', true);
+  }
 });
 
-document.getElementById('hideButton4').addEventListener('click', () => {
+/* document.getElementById('hideButton4').addEventListener('click', () => {
   toggleElement('timer2', false);
-});
+}); */
 
 document.getElementById('toggleButton5').addEventListener('click', () => {
-  toggleElement('timer3', true);
+  if(!document.getElementById('toggleButton5').checked) {
+    toggleElement('timer3', false);
+  } else {
+    toggleElement('timer3', true);
+  }
 });
 
-document.getElementById('hideButton5').addEventListener('click', () => {
+/* document.getElementById('hideButton5').addEventListener('click', () => {
   toggleElement('timer3', false);
-});
+}); */
 
 document.getElementById('toggleButton6').addEventListener('click', () => {
-  toggleElement('timer4', true);
+  if(!document.getElementById('toggleButton6').checked) {
+    toggleElement('timer4', false);
+  } else {
+    toggleElement('timer4', true);
+  }
 });
 
-document.getElementById('hideButton6').addEventListener('click', () => {
+/* document.getElementById('hideButton6').addEventListener('click', () => {
   toggleElement('timer4', false);
-});
+}); */
 
 document.getElementById('toggleButton7').addEventListener('click', () => {
-  toggleElement('timer5', true);
+  if(!document.getElementById('toggleButton7').checked) {
+    toggleElement('timer5', false);
+  } else {
+    toggleElement('timer5', true);
+  }
 });
 
-document.getElementById('hideButton7').addEventListener('click', () => {
+/* document.getElementById('hideButton7').addEventListener('click', () => {
   toggleElement('timer5', false);
-});
+}); */
